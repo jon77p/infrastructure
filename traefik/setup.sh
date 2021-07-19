33d123b278282bd755fa09e1fcc91c3c8dc1609e
Jon Prentice <jon77p@gmail.com>
2021-07-19 12:03:34 -0700

    Added correct ingress definitions

--- traefik/setup.sh
+++ traefik/setup.sh
@@ -3,10 +3,12 @@
 # Pre-requisite: add the following repo
 # helm repo add traefik https://helm.traefik.io/traefik
 
+kubectl apply -f definitions.yaml
+
 helm install traefik traefik/traefik -f deployment-traefik.yaml
 kubectl apply -f deployment-whoami.yaml
 
 kubectl apply -f secrets.yaml
 kubectl apply -f config.yaml
 kubectl apply -f services.yaml
-kubectl apply -f ingress.yaml
+kubectl create --save-config -f ingress.yaml
--- traefik/uninstall.sh
+++ traefik/uninstall.sh
@@ -6,5 +6,16 @@ kubectl delete deployment.apps/whoami
 kubectl delete secret/cloudflare-credentials
 kubectl delete secret/traefik-credentials
 kubectl delete configmap/traefik-config
-kubectl delete ingressroute.traefik.containo.us/traefik-dashboard
+kubectl delete ingressroute.traefik.containo.us/traefik
 kubectl delete ingressroute.traefik.containo.us/whoami
+
+kubectl delete customresourcedefinition.apiextensions.k8s.io/ingressroutes.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/middlewares.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/ingressroutetcps.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/ingressrouteudps.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/tlsoptions.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/tlsstores.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/traefikservices.traefik.containo.us
+kubectl delete customresourcedefinition.apiextensions.k8s.io/serverstransports.traefik.containo.us
+kubectl delete clusterrole.rbac.authorization.k8s.io/traefik-ingress-controller
+kubectl delete clusterrolebinding.rbac.authorization.k8s.io/traefik-ingress-controller
