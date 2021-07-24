8d529089eb502afa318fdb3e6058d3e45c8bd733
Jon Prentice <jon77p@gmail.com>
2021-07-23 18:15:39 -0700

    Migrated to thepi.cloud

--- cloudflared/uninstall.sh
+++ cloudflared/uninstall.sh
@@ -3,3 +3,4 @@
 kubectl delete deployment.apps/cloudflared
 kubectl delete configmap/cloudflared-config
 kubectl delete configmap/cloudflared-credentials
+kubectl delete secret/cloudflared-origin-cert
