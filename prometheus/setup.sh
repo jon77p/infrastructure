300f91fb33d37709ed85dc098cdad1af9a579ef5
Jon Prentice <jon77p@gmail.com>
2021-07-26 17:35:54 -0700

    Removed unnecessary ingress file for prometheus

--- prometheus/setup.sh
+++ prometheus/setup.sh
@@ -2,12 +2,9 @@
 
 # Pre-requisite: add the following repos
 # helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
 
 kubectl apply -f namespace.yaml
 kubectl apply -f secrets.yaml
 kubectl apply -f config.yaml
 
 helm install prometheus prometheus-community/prometheus -n prometheus -f values.yaml
-
-kubectl apply -f ingress.yaml
--- prometheus/uninstall.sh
+++ prometheus/uninstall.sh
@@ -2,8 +2,6 @@
 
 helm uninstall prometheus -n prometheus
 
-kubectl delete ingress.networking.k8s.io/prometheus -n prometheus
-
 kubectl delete configmap/prometheus-server -n prometheus
 kubectl delete secrets/prometheus-creds -n prometheus
 kubectl delete namespace/prometheus
