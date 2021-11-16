9fbdb33368f0bc0a8c898b6de4915711e2b7ca8d
Jon Prentice <jon77p@gmail.com>
2021-11-15 18:50:36 -0800

    Removed pihole, traefik, and cloudflare-companion docker setups

--- k8s/traefik/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-kubectl delete deployment.apps/whoami -n whoami
-kubectl delete service/whoami -n whoami
-kubectl delete ingress.extensions/whoami -n whoami
-
-kubectl delete namespace/whoami
-
-kubectl delete ingressroute.traefik.containo.us/traefik -n kube-system
