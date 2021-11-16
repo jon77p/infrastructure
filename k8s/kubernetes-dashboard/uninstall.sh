a1beb8e59345d30721c2fd46336c428aa437e928
Jon Prentice <jon77p@gmail.com>
2021-11-16 14:43:09 -0800

    Deleted old dashboard setup

--- k8s/kubernetes-dashboard/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-helm uninstall kubernetes-dashboard -n kubernetes-dashboard
-
-kubectl delete ingress.networking.k8s.io/kubernetesdashboard -n kubernetes-dashboard
-
-kubectl delete namespace/kubernetes-dashboard
