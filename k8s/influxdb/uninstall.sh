22048e4a5cc326503e4e6b4562a97622639a7987
Jon Prentice <jon77p@gmail.com>
2021-11-16 13:04:49 -0800

    Removed unused old k8s setups

--- k8s/influxdb/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-helm uninstall influxdb -n influxdb
-
-kubectl delete secrets/influxdb-admin -n influxdb
-kubectl delete namespace/influxdb
--- k8s/varken/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl delete deployment.apps/varken -n varken
-kubectl delete configmap/varken-config -n varken
-kubectl delete namespace/varken
