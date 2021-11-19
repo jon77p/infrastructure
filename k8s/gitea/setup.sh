cd2553c94b924af08eb37220a8e43ef0d0280b4f
Jon Prentice <jon77p@gmail.com>
2021-11-19 09:43:27 -0800

    Removed unused services

--- k8s/gitea/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pre-requisite: add the following repo
-# helm repo add gitea-charts https://dl.gitea.io/charts/
-
-kubectl apply -f namespace.yaml
-kubectl apply -f secrets.yaml
-
-helm install gitea gitea-charts/gitea -n gitea -f values.yaml
--- k8s/gitea/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-helm uninstall gitea -n gitea
-
-kubectl delete secret/gitea-admin-credentials -n gitea
