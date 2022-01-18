0ed57dd940394bb00e92c476b1e276748cdb0809
Jon Prentice <jon77p@gmail.com>
2022-01-17 16:19:38 -0800

    Fixed to retrieve all boot_volumes with same name and AVAILABLE state (#79)
    * Don't delete boot volume for all current instances
    
    * Added output of boot volume for instance, and list all boot volumes in container
    
    * Added outputs for all levels for boot volumes
    
    * Added horrible code to use boot volume if exists or image id otherwise
    
    * Iterate through block volumes by idx, vol
    
    * Renamed boot_volumes to all_boot_volumes, correctly access exported attribute
    
    * Switched to try and use actual boot volume if exists
    
    * Added comments to document magic one-liners
    
    * Fixed to retrieve all boot_volumes with same name and AVAILABLE state

--- terraform/oci/modules/compute/main.tf
+++ terraform/oci/modules/compute/main.tf
@@ -31,15 +31,15 @@ resource "oci_core_instance" "ubuntu_instance" {
 
 
   source_details {
-    # Check to see if there exists a boot volume that starts with the each.value.name hostname
+    # Check to see if there exists a boot volume that starts with the each.value.name hostname and is in the AVAILABLE state
     # if exists, then use that boot volume's id
     # otherwise, use the variable image_id value
-    source_id = length([for idx, vol in data.oci_core_boot_volumes.all_boot_volumes.boot_volumes : vol if split(" ", vol.display_name)[0] == each.value.name]) == 1 ? [for idx, vol in data.oci_core_boot_volumes.all_boot_volumes.boot_volumes : vol if split(" ", vol.display_name)[0] == each.value.name][0].id : each.value.image_id
+    source_id = length([for idx, vol in data.oci_core_boot_volumes.all_boot_volumes.boot_volumes : vol if split(" ", vol.display_name)[0] == each.value.name && vol.state == "AVAILABLE"]) == 1 ? [for idx, vol in data.oci_core_boot_volumes.all_boot_volumes.boot_volumes : vol if split(" ", vol.display_name)[0] == each.value.name && vol.state == "AVAILABLE"][0].id : each.value.image_id
 
-    # Check to see if there exists a boot volume that starts with the each.value.name hostname
+    # Check to see if there exists a boot volume that starts with the each.value.name hostname and is in the AVAILABLE state
     # if exists, then set source_type to "bootVolume"
     # otherwise, set source_type to "image"
-    source_type = length([for vol in data.oci_core_boot_volumes.all_boot_volumes.boot_volumes[*].display_name : vol if split(" ", vol)[0] == each.value.name]) == 1 ? "bootVolume" : "image"
+    source_type = length([for idx, vol in data.oci_core_boot_volumes.all_boot_volumes.boot_volumes : vol if split(" ", vol.display_name)[0] == each.value.name && vol.state == "AVAILABLE"]) == 1 ? "bootVolume" : "image"
   }
 
   # Optional
