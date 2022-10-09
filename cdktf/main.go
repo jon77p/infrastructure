b797fcfe545c01d22022f6b3b861fe8dbca95909
Jon Prentice <jon77p@gmail.com>
2022-10-09 05:24:27 +0000

    Remove some hardcoded values

--- cdktf/main.go
+++ cdktf/main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
-	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/aws/jsii-runtime-go"
-	"github.com/hashicorp/terraform-cdk-go/cdktf"
+    "github.com/aws/constructs-go/constructs/v10"
+    "github.com/aws/jsii-runtime-go"
+    "github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
 func NewTFStack(scope constructs.Construct, id string) cdktf.TerraformStack {
-	stack := cdktf.NewTerraformStack(scope, &id)
+    stack := cdktf.NewTerraformStack(scope, &id)
 
-	// The code that defines your stack goes here
+    // The code that defines your stack goes here
 
-	return stack
+    return stack
 }
 
 func main() {
-	app := cdktf.NewApp(nil)
+    app := cdktf.NewApp(nil)
 
-	stack := NewTFStack(app, "cdktf")
-	cdktf.NewRemoteBackend(stack, &cdktf.RemoteBackendProps{
-		Hostname:     jsii.String("app.terraform.io"),
-		Organization: jsii.String("jon77p-xyz"),
-		Workspaces:   cdktf.NewNamedRemoteWorkspace(jsii.String("infrastructure")),
-	})
+    organization := jsii.String("jon77p-xyz")
+    workspace := cdktf.NewNamedRemoteWorkspace(jsii.String("infrastructure"))
+    hashicorpCloudHostname := jsii.String("app.terraform.io")
+    hashicorpCloud := jsii.String("hashicorp-cloud")
 
-	app.Synth()
+    stack := NewTFStack(app, "cdktf")
+    cdktf.NewRemoteBackend(stack, &cdktf.RemoteBackendProps{
+        Hostname:     hashicorpCloudHostname,
+        Organization: organization,
+        Workspaces:   workspace,
+    })
+
+    remoteState := cdktf.NewDataTerraformRemoteState(stack, hashicorpCloud, &cdktf.DataTerraformRemoteStateRemoteConfig{
+        Workspaces:   workspace,
+        Organization: organization,
+    })
+
+    oci := remoteState.Get(jsii.String("oci"))
+
+    cdktf.NewTerraformOutput(stack, jsii.String("oci"), &cdktf.TerraformOutputConfig{
+        Value: oci,
+    })
+
+    app.Synth()
 }
