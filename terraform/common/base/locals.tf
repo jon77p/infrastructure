f8ebbf07aa71435f9d66591bb705d218759b28d7
Jon Prentice <jon77p@gmail.com>
2022-11-24 21:57:24 +0000

    Removed terraform project configuration

--- terraform/common/base/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  icmp_protocol = 1
-  tcp_protocol  = 6
-  all_protocols = "all"
-
-  anywhere = "0.0.0.0/0"
-
-  ssh_port = 22
-}
--- terraform/getTenancyOCID.sh
+++
@@ -1,1 +0,0 @@
-oci iam user list | jq '.data | map(.["compartment-id"]) | unique | flatten | .[0]' | tr -d '\"'
