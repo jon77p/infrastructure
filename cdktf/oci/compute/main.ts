f6e5f803b24bcff01f95a213687b034c7fd5cf83
Jon Prentice <jon77p@gmail.com>
2022-11-25 20:26:51 +0000

    Another attempt to get Fn.lengthOf into rendered cdktf

--- cdktf/oci/compute/main.ts
+++ cdktf/oci/compute/main.ts
@@ -84,12 +84,13 @@ export class Compute extends Construct {
     )
 
     // Get the number of boot volumes
-    const bootVolumeCount = Fn.lengthOf(this.bootVolumes.bootVolumes)
+    const bootVolumeCount = Fn.lengthOf(
+      Token.asList(this.bootVolumes.bootVolumes)
+    )
 
     // Use the boot volume from the previous run if it exists, otherwise use the instance image
-    const sourceType = `${Token.asString(
-      Fn.lengthOf(this.bootVolumes.bootVolumes) > 0 ? "bootVolume" : "image"
-    )}`
+    const sourceType =
+      "${" + bootVolumeCount + ` > 0 ? "bootVolume" : "image")}`
     const sourceId = `${
       bootVolumeCount > 0
         ? this.bootVolumes.bootVolumes.get(0).id
