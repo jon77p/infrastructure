79d500e1f03a8ff0b216c4bb05264a62502badb2
Jon Prentice <4913666+jon77p@users.noreply.github.com>
2023-07-10 12:48:59 -0700

    chore(ansible): removed all code host references

--- migrate-todo/code-server/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Pre-requisite: run `git submodule init; git submodule update`
-
-kubectl apply -f namespace.yaml
-
-helm install code-server ./code-server/ci/helm-chart -n code-server -f values.yaml
