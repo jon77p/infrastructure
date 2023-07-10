9cff6739d9bb7a7680210e97c6a4ea8bc0f2187d
Jon Prentice <4913666+jon77p@users.noreply.github.com>
2023-07-10 14:07:34 -0700

    chore(ansible/getVaultPassword): fetch with op cli if env var is not set

--- ansible/getVaultPassword.sh
+++ ansible/getVaultPassword.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
+
+# If the ANSIBLE_VAULT_PASSWORD variable isn't set, fetch it using the op cli
+if [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
+	export ANSIBLE_VAULT_PASSWORD=$(op read op://Infrastructure/ansible/vault/password)
+fi
+
 echo $ANSIBLE_VAULT_PASSWORD
