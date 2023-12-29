f0f516b79ad7e3ac2c3b82fa37b6dd1e7e65c8cf
Jon Prentice <4913666+jon77p@users.noreply.github.com>
2023-12-29 15:35:57 -0800

    chore(todo): removed homeassistant

--- migrate-todo/homeassistant/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-helm uninstall home-assistant -n homeassistant
-
-kubectl delete configmap/homeassistant-config -n homeassistant
-kubectl delete ingress.networking.k8s.io/homeassistant -n homeassistant
-
-kubectl delete namespace/homeassistant
