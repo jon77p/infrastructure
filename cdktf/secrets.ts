129cefc87d1f481f8c88b0e6b502994a6cfccbd1
Jon Prentice <4913666+jon77p@users.noreply.github.com>
2023-12-29 20:19:45 -0800

    testing

--- cdktf/secrets.ts
+++ cdktf/secrets.ts
@@ -5,7 +5,23 @@ import * as cdktf from "cdktf";
 import { Construct } from "constructs";
 
 function Setup1Password(scope: Construct) {
-  const command = 'curl -sSfo op.zip "https://cache.agilebits.com/dist/1P/op2/pkg/$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo \'[0-9]+\.[0-9]+\.[0-9]+\')/op_linux_amd64_v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo \'[0-9]+\.[0-9]+\.[0-9]+\')".zip && unzip -od /usr/local/bin/ op.zip && rm op.zip'
+  const arch = "amd64"
+
+  // Fetch 1Password CLI version with typescript
+  const version = fetch("https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N").then((response) => {
+    // Parse version from response as JSON
+    return response.json().then((json: any) => {
+      // Check if version is available
+      if (!json["version"]) {
+        throw new Error("No version found");
+      }
+
+      // Return version
+      return json["version"];
+    });
+  });
+
+  const command = `curl -sSfo op.zip "https://cache.agilebits.com/dist/1P/op2/pkg/v${version}/op_linux_${arch}_v${version}.zip" && unzip -od /usr/local/bin/ op.zip && rm op.zip`
 
   // const opPath = "tools/op"
   const install = new localExec.LocalExec(scope, "1password-install", {
