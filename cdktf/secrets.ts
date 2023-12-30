75eaf0671fded5d9de73ccd108c2cd7dee45e352
Jon Prentice <4913666+jon77p@users.noreply.github.com>
2023-12-29 19:05:33 -0800

    feat(cdktf): use 1Password for secrets management

---
+++ cdktf/secrets.ts
@@ -0,0 +1,67 @@
+import * as onepassword from "./.gen/providers/onepassword"
+import * as localExec from "cdktf-local-exec";
+import * as cdktf from "cdktf";
+
+import { Construct } from "constructs";
+
+function Setup1Password(scope: Construct) {
+  const opPath = "tools/op"
+  const install = new localExec.LocalExec(scope, "1password-install", {
+    cwd: "tools",
+    command: `ARCH="amd64"; \
+      OP_VERSION="v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"; \
+      curl -sSfo op.zip \
+      https://cache.agilebits.com/dist/1P/op2/pkg/"$OP_VERSION"/op_linux_"$ARCH"_"$OP_VERSION".zip \
+      && unzip -od . op.zip \
+      && rm op.zip \
+      && chmod 0755 op \
+      && export PATH="$PATH:$(pwd)/tools" \
+      && echo $PATH \
+      && op --version`
+  });
+
+  return {
+    path: opPath,
+    install: install
+  }
+}
+
+export interface SecretsConfig {
+}
+
+export class Secrets extends Construct {
+  constructor(scope: Construct, name: string, _: SecretsConfig) {
+    super(scope, name)
+
+    // Initialize local-exec provider
+    new localExec.Provider(this, "local-exec");
+
+    // Setup 1Password
+    const setup = Setup1Password(this)
+
+    // Initialize 1Password provider
+    const opProvider = new onepassword.provider.OnepasswordProvider(this, "onepassword", {
+      serviceAccountToken: process.env.OP_SERVICE_ACCOUNT_TOKEN,
+      opCliPath: cdktf.Fn.join("/", [setup.install.cwd, "op"]),
+    });
+
+    // Make sure opProvider depends on the install
+    opProvider.node.addDependency(setup.install);
+
+    // Fetch the 1Password Infrastructure vault
+    const vault = new onepassword.dataOnepasswordVault.DataOnepasswordVault(this, "vault", {
+      name: "Infrastructure",
+    });
+
+    // Fetch the 1Password item for the Terraform login
+    const item = new onepassword.dataOnepasswordItem.DataOnepasswordItem(this, "item", {
+      vault: vault.id,
+      title: "terraform",
+    });
+
+    // Create output for the result
+    new cdktf.TerraformOutput(this, "item-output", {
+      value: item.title,
+    });
+  }
+}
