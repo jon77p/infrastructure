b1af17387b458d7d3b0b9bb7d268610464915c26
Jon Prentice <4913666+jon77p@users.noreply.github.com>
2023-12-29 20:10:09 -0800

    testing

--- cdktf/secrets.ts
+++ cdktf/secrets.ts
@@ -5,15 +5,12 @@ import * as cdktf from "cdktf";
 import { Construct } from "constructs";
 
 function Setup1Password(scope: Construct) {
+  const command = 'curl -sSfo op.zip "https://cache.agilebits.com/dist/1P/op2/pkg/$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo \'[0-9]+\.[0-9]+\.[0-9]+\')/op_linux_amd64_v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo \'[0-9]+\.[0-9]+\.[0-9]+\')".zip && unzip -od /usr/local/bin/ op.zip && rm op.zip'
+
   // const opPath = "tools/op"
   const install = new localExec.LocalExec(scope, "1password-install", {
     cwd: ".",
-    command: `ARCH="amd64"; \
-OP_VERSION="v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"; \
-curl -sSfo op.zip \
-https://cache.agilebits.com/dist/1P/op2/pkg/"$OP_VERSION"/op_linux_"$ARCH"_"$OP_VERSION".zip \
-&& unzip -od /usr/local/bin/ op.zip \
-&& rm op.zip`,
+    command: command,
   });
 
   return {
